4cd0c25b42b49b459d48de92aaf556a892dc0347
Yullit <yuliok03@gmail.com>
2024-11-24 14:36:35 +0200

    make build

---
+++ index.08e46987.js
@@ -0,0 +1,2 @@
+var e=document.getElementsByClassName("logo")[0],t=new Promise(function(t){e.addEventListener("click",function(){return t("Promise was resolved!")})}),n=new Promise(function(e,t){setTimeout(function(){t(Error("Promise was rejected!"))},3e3)});t.then(function(e){var t=document.createElement("div");t.classList.add("message"),t.textContent=e,document.body.appendChild(t)}),n.catch(function(e){var t=document.createElement("div");t.classList.add("message","error-message"),t.textContent=e,document.body.appendChild(t)});
+//# sourceMappingURL=index.08e46987.js.map
